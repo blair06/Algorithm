f48de8f45eb36655faf1a7ed6d3fb60232d7086b
Hyewon <hyewon980610@gmail.com>
2020-08-24 02:50:44 +0900

    Create 19532.cpp

---
+++ 19532.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a, b, c, d, e, f,x,y;
+    cin >> a >> b >> c >> d >> e >> f;
+
+    for (int x = -999; x <= 999; x++) {
+        for (int y = -999; y <= 999; y++) {
+            if (((a * x) + (b * y) == c ) && ((d * x) + (e * y) == f)) {
+                cout << x << " " << y;
+            }
+        }
+    }
+    return 0;
+}
